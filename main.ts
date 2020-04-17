fa76b669f89c4ac3af7fc547c27ca0da20a2de08
Anderson Santos <adsantos@gmail.com>
2020-04-17 19:37:10 -0300

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,31 @@
+controller.A.onEvent(ControllerButtonEvent.Released, function () {
+    for (let index = 0; index < 4; index++) {
+        mySprite.startEffect(effects.trail)
+    }
+})
+let mySprite: Sprite = null
+mySprite = sprites.create(img`
+. . . . . . . . . . b 5 b . . . 
+. . . . . . . . . b 5 b . . . . 
+. . . . . . b b b b b b . . . . 
+. . . . . b b 5 5 5 5 5 b . . . 
+. . . . b b 5 d 1 f 5 5 d f . . 
+. . . . b 5 5 1 f f 5 d 4 c . . 
+. . . . b 5 5 d f b d d 4 4 . . 
+. b b b d 5 5 5 5 5 4 4 4 4 4 b 
+b d d d b b d 5 5 4 4 4 4 4 b . 
+b b d 5 5 5 b 5 5 5 5 5 5 b . . 
+c d c 5 5 5 5 d 5 5 5 5 5 5 b . 
+c b d c d 5 5 b 5 5 5 5 5 5 b . 
+. c d d c c b d 5 5 5 5 5 d b . 
+. . c b d d d d d 5 5 5 b b . . 
+. . . c c c c c c c c b b . . . 
+. . . . . . . . . . . . . . . . 
+`, SpriteKind.Player)
+game.onUpdate(function () {
+    if (mySprite.x <= scene.screenWidth()) {
+        mySprite.x += 1
+    } else {
+        mySprite.x = 0
+    }
+})
